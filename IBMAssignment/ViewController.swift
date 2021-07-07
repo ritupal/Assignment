7cbb6cf9851b889e2a31b5a27db5eb56c92df216
Ritu pal <ritugrd4@gmail.com>
2021-07-07 11:23:12 +0530

    Initial Commit

---
+++ IBMAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IBMAssignment
+//
+//  Created by Ritu pal on 07/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
